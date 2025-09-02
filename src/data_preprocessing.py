d71549460b1d750574093708895994d4de9eaaa1
Sudarshan Gorakhnath Phule <sgphule@gmail.com>
2025-09-02 21:03:27 +0200

    create second component of the pipeline - data preprocessing

---
+++ src/data_preprocessing.py
@@ -0,0 +1,100 @@
+import logging
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import os
+import pandas as pd
+import string
+from sklearn.preprocessing import LabelEncoder
+
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+log_directory = 'logfiles'
+os.makedirs(log_directory, exist_ok=True)
+
+logger = logging.getLogger('data_preprocessing')
+logger.setLevel("DEBUG")
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel("DEBUG")
+
+log_file_path = os.path.join(log_directory, 'data_preprocessing.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel("DEBUG")
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def transform_text(text):
+    """
+    Transforms the input text by converting it to lowercase, tokenizing, removing stopwords and punctuation, and stemming.
+    """
+    ps = PorterStemmer()
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    text = [word for word in text if word.isalnum()]
+    text = [word for word in text if word not in stopwords.words('english') and word not in string.punctuation]
+    text = [ps.stem(word) for word in text]
+    return " ".join(text)
+
+
+def preprocess_df(df, text_column='text', target_column='target'):
+    """
+    Preprocesses the DataFrame by encoding the target column, removing duplicates, and transforming the text column.
+    """
+    try:
+        logger.debug('Starting preprocessing for DataFrame')
+        encoder = LabelEncoder()
+        df[target_column] = encoder.fit_transform(df[target_column])
+        logger.debug('Target column encoded')
+
+        df = df.drop_duplicates(keep='first')
+        logger.debug('Duplicates removed')
+
+        df.loc[:, text_column] = df[text_column].apply(transform_text)
+        logger.debug('Text column transformed')
+        return df
+
+    except KeyError as e:
+        logger.error('Column not found: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Error during text normalization: %s', e)
+        raise
+
+if __name__ == '__main__':
+    try:
+        # Fetch the data from data/raw
+        train_data = pd.read_csv('./data/raw/train.csv')
+        test_data = pd.read_csv('./data/raw/test.csv')
+        text_column = "text"
+        target_column = "target"
+        logger.debug('Data loaded properly')
+
+        # Transform the data
+        train_processed_data = preprocess_df(train_data, text_column, target_column)
+        test_processed_data = preprocess_df(test_data, text_column, target_column)
+
+        # Store the data inside data/processed
+        data_path = os.path.join("./data", "interim")
+        os.makedirs(data_path, exist_ok=True)
+
+        train_processed_data.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
+        test_processed_data.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
+
+        logger.debug('Processed data saved to %s', data_path)
+    except FileNotFoundError as e:
+        logger.error('File not found: %s', e)
+    except pd.errors.EmptyDataError as e:
+        logger.error('No data: %s', e)
+    except Exception as e:
+        logger.error('Failed to complete the data transformation process: %s', e)
